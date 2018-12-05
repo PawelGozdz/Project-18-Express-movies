6c04bd4946f2337708d7a220193eb705ce0a63b7
Pawel Gozdz <pawel.gozdz.web.dev@gmail.com>
2018-12-05 18:20:16 +0100

    deployment

--- secret/database.js
+++ secret/database.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('d18q26o3b6apki', 'ugyckjonayusuo', '9c17168f586979062b861df23449ea36a8289de21abf64636526a7c2d8c56593', {
+const sequelize = new Sequelize('dav4cufv2cm2du', 'umwcgqckopqehx', 'ba71ebf5a9dd1675f4953217eaa5107ed09738bb9a1ccb27b8fbc46d2f62ee16', {
   dialect: 'postgres', host: 'ec2-79-125-26-222.eu-west-1.compute.amazonaws.com'
 });
 
