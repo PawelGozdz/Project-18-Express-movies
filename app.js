273493135aaaf0464625fc7b40e115274a17cab7
Pawel Gozdz <pawel.gozdz.web.dev@gmail.com>
2018-12-05 17:48:21 +0100

    before deploy to heroku

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 const path = require('path');
 
-const port = process.env.PORT || 3000;
-
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -30,6 +28,6 @@ sequelize
   .sync({ force: true })
   // .sync()
   .then(() => {
-    app.listen(port);
+    app.listen(process.env.PORT || 3000);
   })
   .catch(err => console.log(err));
