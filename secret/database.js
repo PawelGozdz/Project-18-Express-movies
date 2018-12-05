5b61e57151133254bea13e44dcf77c4d4b2e1651
Pawel Gozdz <pawelgozdz83@gmail.com>
2018-12-05 18:09:55 +0100

    Delete database.js

--- secret/database.js
+++
@@ -1,7 +0,0 @@
-const Sequelize = require('sequelize');
-
-const sequelize = new Sequelize('dav4cufv2cm2du', 'umwcgqckopqehx', 'ba71ebf5a9dd1675f4953217eaa5107ed09738bb9a1ccb27b8fbc46d2f62ee16', {
-  dialect: 'postgres', host: 'ec2-79-125-26-222.eu-west-1.compute.amazonaws.com'
-});
-
-module.exports = sequelize;
